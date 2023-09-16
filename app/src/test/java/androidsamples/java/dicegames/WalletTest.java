5051cda7814f6413bb8308ea26621df04b6a5864
TheRandomizer7 <yash.yelmame@gmail.com>
2023-09-16 16:01:53 +0530

    Added 2 Unit tests

--- app/src/test/java/androidsamples/java/dicegames/WalletTest.java
+++ app/src/test/java/androidsamples/java/dicegames/WalletTest.java
@@ -1,8 +1,15 @@
 package androidsamples.java.dicegames;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
+import androidx.core.widget.TextViewCompat;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,48 @@ import org.junit.Test;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class WalletTest {
+  @Mock
+  private Die6 die;
+
+  @InjectMocks
+  private WalletViewModel walletViewModel;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
+
   @Test
-  public void addition_isCorrect() {
-    assertEquals(4, 2 + 2);
+  public void dice_rolled_3_5() {
+    when(die.value()).thenReturn(3);
+
+    walletViewModel.rollDie();
+    assertEquals(3, walletViewModel.dieValue());
+
+    when(die.value()).thenReturn(5);
+
+    walletViewModel.rollDie();
+    assertEquals(5, walletViewModel.dieValue());
+  }
+
+  @Test
+  public void dice_rolled_3_6_5_5_6_check_values() {
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(5);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(5);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+
+    assertEquals(5, walletViewModel.balance());
+    assertEquals(5, walletViewModel.previousRoll());
+    assertEquals(2, walletViewModel.singleSixes());
+    assertEquals(5, walletViewModel.totalRolls());
+    assertEquals(0, walletViewModel.doubleSixes(), 0);
+    assertEquals(1, walletViewModel.doubleOthers());
   }
 }
