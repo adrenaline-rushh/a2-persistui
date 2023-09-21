7f9f86949d16045af32fe3993e4428b2d2cc056b
adrenaline-rushh <raghavanaaditya07@gmail.com>
2023-09-21 11:01:39 +0530

    Added more unit tests

--- app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
+++ app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
@@ -1,14 +1,21 @@
 package androidsamples.java.dicegames;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-import android.content.Context;
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +24,54 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class WalletInstrumentedTest {
+  @Rule
+  public ActivityScenarioRule<WalletActivity> activityRule = new ActivityScenarioRule<>(WalletActivity.class);
+
+  @Mock
+  private Die6 die;
+
+  @InjectMocks
+
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
+
   @Test
-  public void useAppContext() {
-    // Context of the app under test.
-    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-    assertEquals("androidsamples.java.dicegames", appContext.getPackageName());
+  public void dice_rolls_66633616654() {
+    when(die.value()).thenReturn(6);
+    onView(withId(R.id.btn_die)).perform(click());
+    assertEquals(5, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    onView(withId(R.id.btn_die)).perform(click());
+    assertEquals(15, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    assertEquals(20, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(1);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(30, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
+    when(die.value()).thenReturn(5);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
+    when(die.value()).thenReturn(4);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
   }
 }
--- app/src/test/java/androidsamples/java/dicegames/WalletTest.java
+++ app/src/test/java/androidsamples/java/dicegames/WalletTest.java
@@ -1,9 +1,7 @@
 package androidsamples.java.dicegames;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
-import androidx.core.widget.TextViewCompat;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -59,6 +57,102 @@ public class WalletTest {
     assertEquals(2, walletViewModel.singleSixes());
     assertEquals(5, walletViewModel.totalRolls());
     assertEquals(0, walletViewModel.doubleSixes(), 0);
+    assertEquals(1, walletViewModel.doubleOthers());
+  }
+
+  @Test
+  public void dice_roll_2166561633() {
+    when(die.value()).thenReturn(2);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(1);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(5);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(1);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+
+    assertEquals(20, walletViewModel.balance());
+    assertEquals(3, walletViewModel.previousRoll());
+    assertEquals(4, walletViewModel.singleSixes());
+    assertEquals(10, walletViewModel.totalRolls());
+    assertEquals(1, walletViewModel.doubleSixes(), 0);
+    assertEquals(1, walletViewModel.doubleOthers());
+  }
+
+  @Test
+  public void dice_rolled_2_1_3_3_4_check_values() {
+    when(die.value()).thenReturn(2);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(1);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    when(die.value()).thenReturn(4);
+    walletViewModel.rollDie();
+
+    assertEquals(-5, walletViewModel.balance());
+    assertEquals(3, walletViewModel.previousRoll());
+    assertEquals(0, walletViewModel.singleSixes());
+    assertEquals(5, walletViewModel.totalRolls());
+    assertEquals(0, walletViewModel.doubleSixes(), 0);
+    assertEquals(1, walletViewModel.doubleOthers());
+  }
+
+  @Test
+  public void dice_roll_66633616654() {
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(5, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(15, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(3);
+    walletViewModel.rollDie();
+    assertEquals(20, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(1);
+    walletViewModel.rollDie();
+    assertEquals(25, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(30, walletViewModel.balance());
+    when(die.value()).thenReturn(6);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
+    when(die.value()).thenReturn(5);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
+    when(die.value()).thenReturn(4);
+    walletViewModel.rollDie();
+    assertEquals(40, walletViewModel.balance());
+
+    assertEquals(40, walletViewModel.balance());
+    assertEquals(5, walletViewModel.previousRoll());
+     assertEquals(6, walletViewModel.singleSixes());
+    assertEquals(11, walletViewModel.totalRolls());
+    assertEquals(3, walletViewModel.doubleSixes(), 0);
     assertEquals(1, walletViewModel.doubleOthers());
   }
 }
